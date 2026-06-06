c47741a1e60f6993125004e52c572afeacad5ad0
oboyle3 <oboyle3patrick@gmail.com>
2026-06-06 13:27:46 -0400

    added some functionaily to main page and linked with backend

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import BlackCard
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-	return render(request, 'index.html')
+	black_card = BlackCard.objects.all()
+	return render(request, 'index.html',{'black_card':black_card})
+
 
 
